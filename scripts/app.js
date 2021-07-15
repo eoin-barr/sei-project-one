985283119a59e249bf9349cde2e785b0b3bc7b89
eoin-barr <eoin.barr@ucdconnect.ie>
2021-07-15 22:02:56 +0100

    HTML, CSS and JS grid implemented

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,41 @@
-console.log('hiya this is the start')
+//* DOM VARIABLES
+const grid = document.querySelector('.grid')
+
+
+
+
+
+
+//* GRID VARIABLES
+const width = 10
+const gridCellCount = width * width
+const cells = []
+
+
+
+
+
+//* GAMES VARIABLES
+
+
+
+
+
+
+
+//* FUNCTIONS
+function createGrid() {
+  for (let i = 0; i < gridCellCount; i++) {
+    const cell = document.createElement('div')
+    cell.setAttribute('data-index', i)
+    cells.push(cell)
+    grid.appendChild(cell)
+  }
+}
+createGrid()
+
+
+
+
+
+//* EVENTS
