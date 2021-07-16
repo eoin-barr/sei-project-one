10d6dea0420a4dd8a854da3b4e20e3b0969f1044
eoin-barr <eoin.barr@ucdconnect.ie>
2021-07-16 15:38:35 +0100

    Got the initial block to fall using setInterval as well as have it be able to move side to side and up and down and remain in place when it reaches the bottom row

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 //* DOM VARIABLES
 const grid = document.querySelector('.grid')
-
+const resetGame = document.querySelector('.reset')
 
 
 
@@ -16,8 +16,7 @@ const cells = []
 
 
 //* GAMES VARIABLES
-
-
+let blockPosition = 5
 
 
 
@@ -34,8 +33,73 @@ function createGrid() {
 }
 createGrid()
 
+function addBlock(position) {
+  cells[position].classList.add('block')
+}
+
+function removeBlock(position) {
+  cells[position].classList.remove('block')
+}
+
+
+function fallingInterval() {
+  const intervalId = setInterval(() => {
+    blockPosition = blockPosition + width
+    removeBlock(blockPosition - width)
+    addBlock(blockPosition)
+    const y = Math.floor(blockPosition / width)
+    if (y === width - 1) {
+      clearInterval(intervalId)
+      // return isFalling = window.confirm('Would you like another block?')
+    }
+  }, 1000)
+}
+fallingInterval()
+
+
+function handleKeyUp(event) {
+  removeBlock(blockPosition) // * remove block from the current position
+  const x = blockPosition % width
+  const y = Math.floor(blockPosition / width)
+
+  if (y < width - 1) {
+    switch (event.keyCode) { // * calculate the next position and update it
+      case 39:
+        if (x < width - 1) blockPosition++
+        break
+      case 37:
+        if (x > 0) blockPosition--
+        break
+      case 38:
+        if (y > 0) blockPosition -= width
+        break
+      case 40:
+        if (y < width - 1) blockPosition += width
+        break
+      default:
+        console.log('invalid key do nothing')
+    }
+    addBlock(blockPosition) // * add block back at the new position
+  } else {
+    let blockToRemain = blockPosition
+    addBlock(blockToRemain)
+    console.log('it works')
+  }
+}
+
+
+
+
+function handleResetGameClick() {
+  let isFalling = true
+  while (isFalling) {
+    fallingInterval()
+  }
+}
 
 
 
 
 //* EVENTS
+resetGame.addEventListener('click', handleResetGameClick)
+window.addEventListener('keyup', handleKeyUp)
